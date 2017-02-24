5bad657eb74d1f165c224c7ce40d64ad6ae72930
Ryan <xjcrazy09@gmail.com>
2017-02-24 12:25:58 -0800

    Initial commit with full modifications done off-line

--- propecia.py
+++ propecia.py
@@ -1,30 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+#############################################################################################
+# This program is meant to check Class C's for a small amount of ports.  In no way is this  #
+# supposed to replace nmap which is very well optimized for a large amount of IPs and Port  #
+# ranges.  This is good for doing a couple of Class C's at a time looking for 1 or 2 ports. #
+#############################################################################################
 
-import sys
-import socket
-import threading
 
+import sys, socket, threading, argparse, re
 
-usage = """
-===========
-propecia.py - A multi-threaded class C network scanner.
-===========
-Created by Michael Allen (http://www.MikeAllen.org)
+# define argument parser #
+parser = argparse.ArgumentParser()
+parser.add_argument('--port','-p',dest="port",help='port help', required=True, nargs="+", type=int)
+parser.add_argument('-V', '--version', action='version', version='%(prog)s (version 0.1)')
 
-Based on the original propecia port scanner written by Bind, available here:
-    http://packetstormsecurity.com/files/14232/propecia.c.html
+# define mutually exclusive group
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--multi','-m',dest="multi",help='Address List help', nargs="?")
+group.add_argument('--addr','-a',dest="addr",help='address help', nargs="+")
+args = parser.parse_args()
 
-Usage: python propecia.py [X.X.X] [Port]
-
-Example: python propecia.py 192.168.0 80 
-"""
-
-if len(sys.argv) <= 2:
-    print usage
-    exit()
-
-ip_prefix = sys.argv[1]
-port = int(sys.argv[2])
 
 # Check if a port is open or not.
 def test_port(ip, port):
@@ -34,21 +28,56 @@ def test_port(ip, port):
         s.connect((str(ip), int(port)))
         s.close
         screenLock.acquire()
-        print ip
+        print ip,port
         screenLock.release()
         return True
     except:
         return False
 
 
+# multi-threading function
 def start_thread(ip, port):
     t = threading.Thread(target=test_port, args=(ip, port))
     t.start()
 
 
-screenLock = threading.Semaphore(value=1)
-   
-for n in range(0,256):
-    ip = ip_prefix + "." + str(n)
-    start_thread(ip, port)
-    
+# iterate through a class C address space
+def class_c(ip_prefix, port):
+	#print "Trying Clas C: %s:%i" % (ip_prefix,port)  #uncomment to debug
+	for n in range(0,256):
+    		ip = ip_prefix + "." + str(n)
+    		start_thread(ip, port)
+
+
+# make sure that the IP is entered in correct format
+def ip_check(ip_prefix, port):
+	try:
+		parts = ip_prefix.split('.')
+		answer = len(parts) == 3 and all(0 <= int(part) < 256 for part in parts)
+		if answer:
+			# if passes the regex send it to the class c function
+			class_c(ip_prefix,port)
+		else:
+			print "%s input should be formated as [X.X.X]" % (ip_prefix)
+	except ValueError:
+		print "One Part is not a convertible to integer"
+	except (AttributeError, TypeError):
+		print "IP address is not a string"
+
+if __name__ == "__main__":
+	# Create screenlock to handle the multi-threading count
+	screenLock = threading.Semaphore(value=1)
+
+	# if text file is passed in parse it.  Otherwise just add the address to the list
+	addresses = []
+	if args.multi is not None:
+		file = open(args.multi, 'r')
+		addresses = [line.rstrip().split(',') for line in file.readlines()]
+	else:
+		addresses = args.addr
+
+	#### Logic time.  Let's go through each port and check the Class C ###
+	for port in args.port:
+		for ip in addresses:
+			ip = ''.join(ip)
+			ip_check(ip, port)
